5b90b783286ea517b609322a552e7affa5ec7dae
serafyn <38337920+serafyn@users.noreply.github.com>
2022-09-09 15:49:37 -0300

    add component banner

--- PortfolioAP/src/app/app.module.ts
+++ PortfolioAP/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
