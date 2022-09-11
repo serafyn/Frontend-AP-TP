90d9016bc7591e576532cf7ae93d633febbde4b8
serafyn <38337920+serafyn@users.noreply.github.com>
2022-09-11 13:11:48 -0300

    add component acercaDe

--- PortfolioAP/src/app/app.module.ts
+++ PortfolioAP/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { LogoAppComponent } from './components/logo-app/logo-app.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SocialComponent } from './components/social/social.component';
     HeaderComponent,
     BannerComponent,
     LogoAppComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
