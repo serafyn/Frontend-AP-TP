cba31a4b5276919b3fbab23d2ecc86ae9b8fd2b3
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-17 09:35:19 -0300

    Entidad persona lista

---
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Service/ImpPersonaService.java
@@ -0,0 +1,36 @@
+package com.portfolioap.backendportfolioap.Service;
+
+import com.portfolioap.backendportfolioap.Entity.Persona;
+import com.portfolioap.backendportfolioap.Interface.IPersonaService;
+import com.portfolioap.backendportfolioap.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonarepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonarepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+           ipersonarepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonarepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonarepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
