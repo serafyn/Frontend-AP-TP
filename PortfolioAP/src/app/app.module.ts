3779894dc9bb7e4de65164502aaaa4bfb81ffe0b
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-20 21:49:20 -0300

    Revert "modelo de persona"
    This reverts commit 4b98f53854fbff0a9b476bd3d0c72deff4a4d6e4.

--- PortfolioAP/src/app/app.module.ts
+++ PortfolioAP/src/app/app.module.ts
@@ -48,15 +48,3 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class persona {
-  id?: number;
-  nombre: string;
-  apellido: string;
-  img: string;
-
-  constructor(nombre: string, apellido: string, img: string) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.img = img;
-  }
-}
