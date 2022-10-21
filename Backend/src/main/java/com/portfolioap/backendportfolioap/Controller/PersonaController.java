3fd2f4fdaa6fe64bea462a7307a6f5b550ee2063
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-20 21:22:43 -0300

    Update de URL controlador persona

--- Backend/src/main/java/com/portfolioap/backendportfolioap/Controller/PersonaController.java
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
