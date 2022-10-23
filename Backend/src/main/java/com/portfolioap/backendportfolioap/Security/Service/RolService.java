2121536998b6293695ad2449cb92473c7ce6e152
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-23 11:08:45 -0300

    Rol Service

--- Backend/src/main/java/com/portfolioap/backendportfolioap/Security/Service/RolService.java
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Security/Service/RolService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolioap.backendportfolioap.Security.Service;
 
 import com.portfolioap.backendportfolioap.Security.Entity.Rol;
