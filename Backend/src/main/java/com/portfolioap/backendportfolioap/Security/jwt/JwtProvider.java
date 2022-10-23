90a785eb5c3fe2bac5da30c11d93589ff18d8343
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-23 15:15:37 -0300

    Up

---
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Security/jwt/JwtProvider.java
@@ -0,0 +1,15 @@
+
+package com.portfolioap.backendportfolioap.Security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtProvider {
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    
+    private String secret;
+    private Int expiration
+}
---
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Security/jwt/jwtEntryPoint.java
@@ -0,0 +1,27 @@
+
+package com.portfolioap.backendportfolioap.Security.jwt;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class jwtEntryPoint implements AuthenticationEntryPoint{
+
+    private final static Logger logger = LoggerFactory.getLogger(jwtEntryPoint.class);
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+      logger.error("Hubo una FALLA en el metodo commence");
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+    
+}
