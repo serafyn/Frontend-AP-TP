7a166fbdb7f753786c152982d831d0468ba4edb5
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-23 10:13:05 -0300

    Listo iRolRepository

--- Backend/src/main/java/com/portfolioap/backendportfolioap/Security/Repository/iRolRepository.java
+++ Backend/src/main/java/com/portfolioap/backendportfolioap/Security/Repository/iRolRepository.java
@@ -1,10 +1,12 @@
 package com.portfolioap.backendportfolioap.Security.Repository;
 
 import com.portfolioap.backendportfolioap.Security.Entity.Rol;
+import com.portfolioap.backendportfolioap.Security.Enums.RolNombre;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface iRolRepository extends JpaRepository<Rol, Integer> {
-    
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
 }
