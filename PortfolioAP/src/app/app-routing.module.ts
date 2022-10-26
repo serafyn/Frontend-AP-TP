91c8ed409f527df519029383757f29a02abf15ca
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-26 11:26:57 -0300

    Agregando ruta

--- PortfolioAP/src/app/app-routing.module.ts
+++ PortfolioAP/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'nuevaexp', component: NewExperienciaComponent }
+
 ];
 
 @NgModule({
