eee6792d2368c0f3bfe52ab650766e027b2bc579
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-26 08:52:04 -0300

    Cambios en el front

--- PortfolioAP/src/app/components/logo-app/logo-app.component.ts
+++ PortfolioAP/src/app/components/logo-app/logo-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-logo-app',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo-app.component.css']
 })
 export class LogoAppComponent implements OnInit {
-
-  constructor() { }
+  isLogged = false;
+  constructor(private router: Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+  onLogOut(): void {
+    this.tokenService.logOut();
+    window.location.reload();
   }
 
+  login() {
+    this.router.navigate(['/login'])
+  }
 }
