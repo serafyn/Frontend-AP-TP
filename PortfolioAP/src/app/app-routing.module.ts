0d2991c06fe2c52cee18a58e92c53e1386dbf313
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-27 07:07:34 -0300

    up

--- PortfolioAP/src/app/app-routing.module.ts
+++ PortfolioAP/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
+// import { EditSkillComponent } from './components/hys/edit-skill.component';
+// import { NewSkillComponent } from './components/hys/new-skill.component';
 import { LoginComponent } from './components/login/login.component';
-import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +16,8 @@ const routes: Routes = [
   { path: 'editexp/:id', component: EditExperienciaComponent },
   { path: 'nuevaedu', component: NeweducacionComponent },
   { path: 'editedu/:id', component: EditeducacionComponent },
+  // { path: 'newskill', component: NewSkillComponent},
+  // { path: 'editskill/:id', component: EditSkillComponent}
 ];
 
 @NgModule({
