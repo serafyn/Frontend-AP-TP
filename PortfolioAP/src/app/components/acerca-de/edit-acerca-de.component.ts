22984a8884f9d7596c003e4cd9e930c40fe795c1
serafyn <38337920+serafyn@users.noreply.github.com>
2022-10-28 08:25:30 -0300

    Frontend funcionando

--- PortfolioAP/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ PortfolioAP/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { persona } from '../../model/persona.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PersonaService } from 'src/app/service/persona.service';
+import { ImageService } from 'src/app/service/image.service';
 
 @Component({
   selector: 'app-edit-acerca-de',
@@ -10,7 +11,7 @@ import { PersonaService } from 'src/app/service/persona.service';
 })
 export class EditAcercaDeComponent implements OnInit {
   persona: persona = null;
-  constructor(private activatedRouter: ActivatedRoute, private personasService: PersonaService, private router: Router) { }
+  constructor(private activatedRouter: ActivatedRoute, private personasService: PersonaService, private router: Router, public imageService: ImageService) { }
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
@@ -27,6 +28,7 @@ export class EditAcercaDeComponent implements OnInit {
 
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url;
     this.personasService.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['']);
@@ -39,6 +41,8 @@ export class EditAcercaDeComponent implements OnInit {
 
 
   uploadImage($event: any) {
-
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name = "perfil_" + id;
+    this.imageService.uploadImage($event, name)
   }
 }
--- PortfolioAP/src/app/service/image.service.ts
+++ PortfolioAP/src/app/service/image.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Storage, ref, uploadBytes, list, getDownloadURL } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
+  url: string = "";
 
-  constructor() { }
+  constructor(private storage: Storage) { }
+
+  uploadImage($event: any, name: string) {
+    const file = $event.target.files[0]
+    const imgRef = ref(this.storage, `imagen/` + name)
+    uploadBytes(imgRef, file)
+      .then(response => { this.getImage() })
+      .catch(error => console.log(error))
+  }
+
+  getImage() {
+    const imagesRef = ref(this.storage, 'imagen')
+    list(imagesRef)
+      .then(async response => {
+        for (let item of response.items) {
+          this.url = await getDownloadURL(item);
+          console.log("Url es" + this.url);
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
 }
